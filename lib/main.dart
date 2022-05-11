2aaa2fffb2dd7a344f244d98ee2d7f8a31e020da
reve74 <reve99@naver.com>
2022-05-11 23:49:51 +0900

    setting

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:video_call/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
